ae3b8b996af175ffbb4b63454ec572a8fd9bc2eb
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2022-06-18 19:47:37 +0800

    CORS: fix cors problem

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ const order = require('./modules/order')
 const patient = require('./modules/patient')
 
 //use
+router.use((req, res, next)=> {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', false)
+    next()
+})
 router.use('/order', order)
 router.use('/patient', patient)
 //error
