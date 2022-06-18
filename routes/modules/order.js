2dbb92b0165f380bcef287dfaedc27d0df5895b1
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2022-06-18 14:09:13 +0800

    Router: Add GET/POST Order

--- routes/modules/order.js
+++ routes/modules/order.js
@@ -6,5 +6,32 @@ const Order = require('../../models/order')
 
 //API base: order
 
+//Get order
+router.get('/', async (req, res) => {
+    try {
+        const { patientId } = req.query
+        const field = '_id message createAt updateAt'
+        const orders = await Order.find({ patientId }, field).lean()
+
+        res.json(orders)
+    } catch(e) {
+        console.error(e)
+        res.status(500).send('Error!')
+    }
+})
+
+//Add order
+router.post('/', async (req, res) => {
+    try {
+        const { message, patientId } = req.body
+        await Order.create({ message, patientId })
+        res.json({
+            status: 'success'
+        })
+    } catch(e) {
+        console.error(e)
+        res.status(500).send('Error!')
+    }
+})
 
 module.exports = router
