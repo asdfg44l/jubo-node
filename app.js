73a42a0e123d26de9290f2dfb7d3fb4d17ed83b8
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2022-06-20 11:33:21 +0800

    CORS: use cors module

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const cors = require("cors")
 const routes = require("./routes")
+
+
 
 //express app
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//cors
+app.use(cors())
 
 //mongoose
 require('./config/mongoose')
--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,6 @@ const order = require('./modules/order')
 const patient = require('./modules/patient')
 
 //use
-router.use((req, res, next)=> {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', false)
-    next()
-})
 router.use('/order', order)
 router.use('/patient', patient)
 //error
